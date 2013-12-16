f0c15eabb1751dd99b3ee361221ef8d776a07346
Joseph <joseph@larmarange.net>
2013-12-16 23:07:01 +0200

    A first support of bibliographic references (NB: need to fix the use of pandoc-zotxt filter)

--- scdoc/scdoc.r
+++ scdoc/scdoc.r
@@ -20,6 +20,7 @@ options(rstudio.markdownToHTML =
 						"--smart",
 						"--number-sections",
             "--mathml",
+            "--filter=pandoc-citeproc",
 						"-S"))
 				if (!is.null(opt$pdf)) if (opt$pdf == 'prince')
 					system(paste("prince",
@@ -27,3 +28,48 @@ options(rstudio.markdownToHTML =
 						"--javascript"))
 			}
 )
+
+#' Extract references from a Rmd/md file with Zotxt and create a JSON file
+#' 
+#' @param file file to parse
+#' @param json name of the JSON file to produce (if NULL automatically determined)
+#' @param return.file.name if TRUE, return the name of the JSON file
+#' @note Zotero should be opened for this function to work.
+
+zotxt2json <- function (file, json = NULL, return.file.name = FALSE) {
+  if (is.null(json)) {
+    base.file <- sub("\\.[a-zA-Z]*$","", file)
+    json <- paste0(base.file,".json")  
+  }
+  json.code <- system(paste(
+    "python ./scdoc/extractcites.py",
+    shQuote(file)
+  ), intern = TRUE)
+  cat(json.code, file=json, sep="\n")
+  if (return.file.name) return(json)
+}
+
+#' Extract references from a Rmd/md file with Zotxt and convert it to YAML
+#' 
+#' @param file file to parse
+#' @param keep if TRUE, keep the json and YAML files produced
+#' @param display if TRUE, display the result in the console
+#' @note Zotero should be opened for this function to work.
+
+zotxt2yaml <- function (file, keep = FALSE, display = TRUE) {
+  base.file <- sub("\\.[a-zA-Z]*$","", file)
+  json.file <- paste0(base.file,".json")
+  yaml.file <- paste0(base.file,".yaml")
+  zotxt2json(file, json.file)
+  yaml <- system(paste(
+    "biblio2yaml",
+    shQuote(json.file)
+    ), intern = TRUE)
+  Encoding(yaml) <- "UTF-8"
+  if (keep)
+    cat(yaml, file=yaml.file, sep="\n")
+  else
+    file.remove (json.file)
+  if (display)
+    cat(yaml, sep="\n")
+}
