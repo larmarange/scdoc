4d9cecfd7d54791afc2ec4b0a08819ddaaae36d8
Joseph <joseph@larmarange.net>
2013-12-18 13:03:30 +0100

    evolution of scdoc() function

--- R/scdoc.r
+++ R/scdoc.r
@@ -1,42 +1,61 @@
-#' Initialize scdoc
+#' Initialize scdoc or knit and convert a Rmd file using scdoc
 #' 
+#' @param file file to knit and convert using scdoc, if NULL initialize scdoc in R Studio
+#' @details Use the command \code{scdoc()} to define scdoc() as the command to 
+#' apply when clicking on \strong{Knit HTML} button in R Studio. Alternativaly,
+#' you can also specify a file to knit and convert into HTML/PDF.
 #' @export scdoc
 
-scdoc <- function() {
-  options(rstudio.markdownToHTML = 
-        function(inputFile, outputFile) {
-          path <- paste0(installed.packages()["scdoc","LibPath"],"/scdoc/scdoc/")
-          if (!require(yaml)) {
-            install.packages("yaml")
-            library("yaml")
-          }
-          f <- readLines(inputFile, encoding="UTF-8")
-          if (!is.na(b <- which(f == "---")[1]) & !is.na(e <- which(f == "...")[1]))
-            opt <- yaml.load(paste(f[(b+1):(e-1)], collapse="\n"))
-          else
-            opt <- list()
-          if (!is.null(opt$zotxt)) zotxt <- "--filter=pandoc-zotxt"
-          else zotxt <- ""
-          system(paste("pandoc", 
-            shQuote(inputFile), 
-            "-t html5 -s -o", 
-            shQuote(outputFile),
-            paste0("--template=", path, "scdoc-template.html5"),
-            paste0("--include-in-header=", path, "scdoc-head.html"),
-            "--self-contained",
-            paste0("--data-dir=", path),
-            "--smart",
-            "--number-sections",
-            "--mathml",
-            zotxt,
-            "--filter=pandoc-citeproc",
-            "-S"))
-          if (!is.null(opt$pdf)) if (opt$pdf == 'prince')
-            system(paste("prince",
-              shQuote(outputFile),
-              "--javascript"))
-        }
-  )
+scdoc <- function(file = NULL) {
+  if (is.null(file))
+    options(rstudio.markdownToHTML = scdoc.md2html)
+  else {
+    mdfile <- paste0(sub("\\.[a-zA-Z]*$","", file),".md")
+    htmlfile <- paste0(sub("\\.[a-zA-Z]*$","", file),".html")
+    knit(file, mdfile)
+    scdoc.md2html(mdfile, htmlfile)
+  }
+}
+
+#' Convert an md file to HTML/PDF
+#' 
+#' @param inputFile input file name (md file)
+#' @param outputFile output file name (html file)
+#' @details the output pdf file name will be derivated from \code{outputFile}.
+#' @export scdoc.md2html
+
+
+scdoc.md2html <- function(inputFile, outputFile) {
+  path <- paste0(installed.packages()["scdoc","LibPath"],"/scdoc/scdoc/")
+  if (!require(yaml)) {
+    install.packages("yaml")
+    library("yaml")
+  }
+  f <- readLines(inputFile, encoding="UTF-8")
+  if (!is.na(b <- which(f == "---")[1]) & !is.na(e <- which(f == "...")[1]))
+    opt <- yaml.load(paste(f[(b+1):(e-1)], collapse="\n"))
+  else
+    opt <- list()
+  if (!is.null(opt$zotxt)) zotxt <- "--filter=pandoc-zotxt"
+  else zotxt <- ""
+  system(paste("pandoc", 
+    shQuote(inputFile), 
+    "-t html5 -s -o", 
+    shQuote(outputFile),
+    paste0("--template=", path, "scdoc-template.html5"),
+    paste0("--include-in-header=", path, "scdoc-head.html"),
+    "--self-contained",
+    paste0("--data-dir=", path),
+    "--smart",
+    "--number-sections",
+    "--mathml",
+    zotxt,
+    "--filter=pandoc-citeproc",
+    "-S"))
+  if (!is.null(opt$pdf)) if (opt$pdf == 'prince')
+    system(paste("prince",
+      shQuote(outputFile),
+      "--javascript"))
 }
 
 #' Extract references from a Rmd/md file with Zotxt and create a JSON file
--- copy_vignettes.R
+++ copy_vignettes.R
@@ -1,2 +1,4 @@
+library(scdoc)
+scdoc("./vignettes/Mode_d_emploi.html")
 file.copy("./vignettes/Mode_d_emploi.html","./inst/doc/Mode_d_emploi.html", overwrite = TRUE)
 file.copy("./vignettes/Mode_d_emploi.pdf","./inst/doc/Mode_d_emploi.pdf", overwrite = TRUE)
